4612de21789f089a1dcf13ae888b8965b2cf475d
Zerostressingonlyfinessing <ruslan.boulevard@yandex.ru>
2025-06-25 17:50:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,4 @@ def add_item():
 
 # Run app
 if __name__ == '__main__':
-    os.makedirs('static/uploads', exist_ok=True)
-    db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
