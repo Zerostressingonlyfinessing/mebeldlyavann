f13157f3a939e4587178c9e1e9c95612802c3062
Zerostressingonlyfinessing <ruslan.boulevard@yandex.ru>
2025-06-25 18:16:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-
 from flask import Flask, render_template
 from models import init_db, get_message
 
 app = Flask(__name__)
 
+# Создаём базу данных при запуске (если её нет)
+init_db()
+
 @app.route('/')
 def index():
     message = get_message()
     return render_template('index.html', message=message)
-
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
